8b3a0d668e54b21a4b8205dd16064a4717d94a2b
STENTORS <147637091+STENTORS@users.noreply.github.com>
2025-06-09 14:06:12 +0100

    header and text to speech

--- static/speech2text.js
+++ static/speech2text.js
@@ -1,12 +1,13 @@
-const textEL = document.getElementById('text');
 const speakEL = document.getElementById('speak');
-
+const textEL = document.getElementById('text');
 speakEL.addEventListener('click', speakText);
 function speakText() {
+    const text = textEL.textContent;
+    alert(text);
     // stop any speaking in progress
     window.speechSynthesis.cancel();
 
-    const text = textEL.value;
     const utterance = new SpeechSynthesisUtterance(text);
-    window.speechSynthesis.speak(text);
+    window.speechSynthesis.speak(utterance);
+
 }
