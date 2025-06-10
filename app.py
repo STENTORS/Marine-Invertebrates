c3c1f656e1b57fe93173d30a757c31f974f5e822
STENTORS <147637091+STENTORS@users.noreply.github.com>
2025-06-10 12:15:47 +0100

    catch api error

--- app.py
+++ app.py
@@ -2,19 +2,25 @@ from flask import Flask
 from flask import render_template
 from flask import redirect
 from flask import url_for
+from flask import request 
 import API
 app = Flask(__name__)
 
 @app.route("/home")
 @app.route("/")
 def home():
-    return render_template("index.html")
+    error = request.args.get("error") 
+    return render_template("index.html", error=error) 
 
 
 @app.route("/collection")
 def collection():
-    data = API.get_marine_invertebrates()
-    return render_template("collection.html", data=data)
+    try:
+        data = API.get_marine_invertebrates()
+        return render_template("collection.html", data=data)
+    except:
+        return redirect(url_for("home", error="Error fetching data from the API."))
+
 
 @app.route("/game")
 def game():
